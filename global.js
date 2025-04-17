a21d34d9c14f3eea3ac05719f894031a19c14a9f
Russel Luber <rluber@ucsd.edu>
2025-04-16 21:28:21 -0700

    step 1 done; hooked up global.js script to all html pages

--- global.js
+++ global.js
@@ -1,7 +1,12 @@
+// global.js
 console.log('IT’S ALIVE!');
 
+/**
+ * $$ — shorthand for document.querySelectorAll()
+ * @param {string} selector — CSS selector
+ * @param {Element|Document} context — where to query (defaults to document)
+ * @returns {Array<Element>}
+ */
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-
-// what the hell
