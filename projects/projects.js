bac76b665b1f3b702ea0b5c8ca8fe6caac903d92
russel-luber <rluber@ucsd.edu>
2025-04-25 14:48:46 -0700

    added project counter at title of proj page

--- projects/projects.js
+++ projects/projects.js
@@ -4,6 +4,14 @@ const loadProjects = async () => {
   const projects = await fetchJSON('../lib/projects.json');
   const projectsContainer = document.querySelector('.projects');
   renderProjects(projects, projectsContainer, 'h2');
+
+  // --- Project Counter --
+  const title = document.querySelector('.projects-title');
+  if (title) {
+    title.textContent = `${projects.length} Projects`;
+  }
+  
+
 };
 
 loadProjects();
