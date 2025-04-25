2b024dfa703d740ad8f306480c17e3efcaba43d2
russel-luber <rluber@ucsd.edu>
2025-04-25 14:07:55 -0700

    projects.js created for rendering proj page

--- projects/projects.js
+++ projects/projects.js
@@ -1,1 +1,9 @@
 import { fetchJSON, renderProjects } from '../global.js';
+
+const loadProjects = async () => {
+  const projects = await fetchJSON('../lib/projects.json');
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(projects, projectsContainer, 'h2');
+};
+
+loadProjects();
